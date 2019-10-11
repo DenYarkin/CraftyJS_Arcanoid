9dfe19c08e6539f7ef14bc2e7db7c2bc3188f91c
S1eap <den.s1eap@gmail.com>
2019-10-11 12:28:30 +0300

    Initial commit

---
+++ src/vars.js
@@ -0,0 +1,24 @@
+let nowTheme = 0;
+let numOfThemes = 2;
+let themes = [
+    ["black", "white"],
+    ["blue", "yellow"]
+]
+let theme_names = ["Classic", "Patriot"]
+
+let gameMode = 0;
+
+let numOfBonuses, maxBonuses = 10, bonusExistTime = 100;
+
+let gameHeight = 500;
+let gameWidth = 700;
+
+let wallWidth = 10;
+
+let boardHeight = 50;
+let boardWidth = 10;
+let circleRadius = 7;
+
+let leftScore, rightScore, endGameScore = 5;
+
+let ballRadius = 10;
